f956e7516cef4a07d31b595aead410ef18424822
Matvey Duryagin <100364663+duriaginmv@users.noreply.github.com>
2022-04-14 22:39:58 +0300

    Add react app

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { FuncComponent } from "./FuncComponent";
+import { ClassComponent} from "./ClassComponent";
+
+function App() {
+  return (
+    <div className="App">
+        <center>
+            <FuncComponent/>
+            <ClassComponent/>
+        </center>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ ClassComponent.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import './ClassComponent.css';
+
+export class ClassComponent extends Component{
+    render(){
+        return (
+            <div>
+                <text1>FuncComponent  </text1>
+                <a href="https://yandex.ru">Yandex   </a>
+                <button> кнопочка </button>
+            </div>
+
+        )
+    }
+}
