ad3fd78c4a42578318924881a0115366d53544b5
Matvey Duryagin <100364663+duriaginmv@users.noreply.github.com>
2022-05-07 19:05:03 +0300

    Lesson 11

--- src/App.js
+++ src/App.js
@@ -1,66 +1,67 @@
 import React from 'react';
-import {useEffect, useReducer} from "react";
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {Asteroids} from './Pages/Asteroid';
-import {Destroy} from './Pages/Destroy';
-import {AsteroidsArray, URL} from "./API";
+import { useEffect, useReducer } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Asteroids } from './Pages/Asteroid';
+import { Destroy } from './Pages/Destroy';
+import { AsteroidsArray, URL } from './API';
 
 export const context = React.createContext(null);
 
 export function App() {
-
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case 'Array':
-        return {
-          ...state,
-          array: action.payload
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'Array':
+                return {
+                    ...state,
+                    array: action.payload,
+                };
+            case 'Destroy':
+                return {
+                    ...state,
+                    destroy: [...state.destroy, action.payload],
+                };
+            case 'Show':
+                return {
+                    ...state,
+                    show: action.payload,
+                };
+            case 'Distance':
+                return {
+                    ...state,
+                    distance: action.payload,
+                };
+            default:
+                throw new Error();
         }
-      case 'Destroy':
-        return {
-          ...state,
-          destroy: [...state.destroy, action.payload]
-        }
-      case 'Show':
-        return {
-          ...state,
-          show: action.payload
-        }
-      case 'Distance':
-        return {
-          ...state,
-          distance: action.payload
-        }
-      default:
-        throw new Error();
-    }
-  }
+    };
 
-  const [state,dispatch]=useReducer(reducer,{
-    array: [],
-    destroy: [],
-    show: false,
-    distance: 0
-  });
+    const [state, dispatch] = useReducer(reducer, {
+        array: [],
+        destroy: [],
+        show: false,
+        distance: 0,
+    });
 
-  useEffect(() => {
-    fetch(URL())
-        .then((response) => response.json())
-        .then((data) => {dispatch({payload:AsteroidsArray(data), type: 'Array'})})
-        .catch((error) => console.log(error))
-  },[]);
+    useEffect(() => {
+        fetch(URL())
+            .then((response) => response.json())
+            .then((data) => {
+                dispatch({ payload: AsteroidsArray(data), type: 'Array' });
+            })
+            .catch((error) => console.log(error));
+    }, []);
 
-  return (
-      <>
-        <context.Provider value={{state:state, dispatch:dispatch}}>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Asteroids />}/>
-              <Route path="/destroy" element={<Destroy />}/>
-            </Routes>
-          </BrowserRouter>
-        </context.Provider>
-      </>
-  );
+    return (
+        <>
+            <context.Provider value={{ state: state, dispatch: dispatch }}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Asteroids />} />
+                        <Route path="/destroy" element={<Destroy />} />
+                    </Routes>
+                </BrowserRouter>
+            </context.Provider>
+        </>
+    );
 }
 export default App;
--- src/Modules/Asteroid/Asteroid.jsx
+++ src/Modules/Asteroid/Asteroid.jsx
@@ -1,52 +1,59 @@
-import styles from'./Asteroid.module.css'
-import smallAsteroid from '../../PNG/SmallAsteroid.png'
-import middleAsteroid from '../../PNG/MiddleAsteroid.png'
-import bigAsteroid from '../../PNG/BigAsteroid.png'
-import dino from '../../PNG/Dino.png'
-import {useContext} from "react";
-import {context} from "../../App";
+import styles from './Asteroid.module.css';
+import smallAsteroid from '../../PNG/SmallAsteroid.png';
+import middleAsteroid from '../../PNG/MiddleAsteroid.png';
+import bigAsteroid from '../../PNG/BigAsteroid.png';
+import dino from '../../PNG/Dino.png';
+import { useContext } from 'react';
+import { context } from '../../App';
 
-export function Asteroid (props) {
-    const {state, dispatch} = useContext(context);
+export function Asteroid(props) {
+    const { state, dispatch } = useContext(context);
 
-    function DestroyArray () {
+    function DestroyArray() {
         dispatch({
-            payload: {name: props.name, date: props.date, distance: props.distance, size: props.size, danger: props.danger, distanceMode: state.distanceMode},
-            type: 'Destroy'
+            payload: {
+                name: props.name,
+                date: props.date,
+                distance: props.distance,
+                size: props.size,
+                danger: props.danger,
+                distanceMode: state.distanceMode,
+            },
+            type: 'Destroy',
         });
     }
 
     return (
         <div className={styles.center}>
-            <div className={props.danger==="не опасен"?"cardGreen":"cardRed"}>
-                <img className={styles.dino} src={dino} alt="Динозавр"/>
-                {props.size<300?
-                    <img className={styles.smallAsteroid} src={smallAsteroid} alt="Маленький астероид"/>
-                    :props.size<600?
-                        <img className={styles.middleAsteroid} src={middleAsteroid} alt="Средний астероид"/>
-                        :
-                        <img className={styles.bigAsteroid} src={bigAsteroid} alt="Большой астероид"/>
-                }
+            <div className={props.danger === 'не опасен' ? 'cardGreen' : 'cardRed'}>
+                <img className={styles.dino} src={dino} alt="Динозавр" />
+                {props.size < 300 ? (
+                    <img className={styles.smallAsteroid} src={smallAsteroid} alt="Маленький астероид" />
+                ) : props.size < 600 ? (
+                    <img className={styles.middleAsteroid} src={middleAsteroid} alt="Средний астероид" />
+                ) : (
+                    <img className={styles.bigAsteroid} src={bigAsteroid} alt="Большой астероид" />
+                )}
                 <label className={styles.name}>{props.name}</label>
                 <label className={styles.description}>
                     <label className={styles.date}>Дата................{props.date}</label>
                     <label className={styles.size}>Размер...................................{props.size} м</label>
-                    {props.distanceMode === 0 ?
+                    {props.distanceMode === 0 ? (
+                        <label className={styles.distance}>Расстояние.................{props.distance} км</label>
+                    ) : (
                         <label className={styles.distance}>
-                            Расстояние.................{props.distance} км
+                            Расстояние.................{(props.distance / 384000).toFixed(2)} лун (в шт)
                         </label>
-                        :
-                        <label className={styles.distance}>
-                            Расстояние.................{(props.distance/384000).toFixed(2)} лун (в шт)
-                        </label>
-                    }
+                    )}
                 </label>
                 <label className={styles.rate}>Оценка:</label>
-                <label className={props.danger==="не опасен" ? styles.rateGreen : styles.rateRed}>
+                <label className={props.danger === 'не опасен' ? styles.rateGreen : styles.rateRed}>
                     {props.danger}
                 </label>
                 <div className={styles.button}>
-                    <button className={styles.destroy} onClick={DestroyArray}>На уничтожение</button>
+                    <button className={styles.destroy} onClick={DestroyArray}>
+                        На уничтожение
+                    </button>
                 </div>
             </div>
         </div>
